dd0043b30c3d467c95e11353b7216c275fa16d41
Daltonist92 <86077963+Daltonist92@users.noreply.github.com>
2021-06-29 23:44:01 +0200

    Add files via upload

---
+++ MongoDB4/zapytanie_3_A.js
@@ -0,0 +1,12 @@
+printjson(
+    db.people.aggregate([
+        {
+            $group: {
+                _id: "$job",
+                count: {
+                    $sum: 1
+                }
+            }
+        }
+    ])
+);
